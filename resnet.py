e64f4d85badf6bd8ca2fd589405145eacb3b764f
CynthiaK <khancynthia28@gmail.com>
2019-05-04 10:33:02 -0400

    Credits to Hvass-Labs.
    This code is boilerplate code for downloading and preprocessing of the CIFAR-10 dataset.

---
+++ resnet.py
@@ -0,0 +1,501 @@
+import numpy as np
+import pickle
+import os
+import urllib
+import zipfile 
+import tarfile
+from six.moves import urllib
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# tensorflow imports
+import tensorflow as tf
+from tensorflow.contrib.training import HParams
+
+# tf.keras imports
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Conv2D, Activation, Flatten
+from tensorflow.keras.layers import BatchNormalization, AveragePooling2D, Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# import for showing the confusion matrix
+import itertools
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Hyper parameters
+hparams = HParams(
+    n_classes=10,  # number of different classes in dataset
+    learning_rate=1e-4,  # fixed learning rate
+    train_batch_size=32,  # training batch size
+    val_batch_size=32,  # validation batch size
+    test_batch_size=32,  # testing batch size
+    n_epochs=10,  # number of epochs to train
+    input_name='input_1',  # name of the input tensor for first layer of Keras model
+    data_dir='/tmp/cifar-data/',  # path to data directory
+    checkpoint_dir='/tmp/checkpoints/'  # path to model checkpoint directory
+)
+
+#Data preprocessing
+# URL for the data-set on the internet.
+data_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+
+# Width and height of each image.
+img_size = 32
+
+# Number of channels in each image, 3 channels: Red, Green, Blue.
+num_channels = 3
+
+# Length of an image when flattened to a 1-dim array.
+img_size_flat = img_size * img_size * num_channels
+
+# Number of classes.
+num_classes = hparams.n_classes
+
+########################################################################
+# Various constants used to allocate arrays of the correct size.
+
+# Number of files for the training-set.
+_num_files_train = 5
+
+# Number of images for each batch-file in the training-set.
+_images_per_file = 10000
+
+# Total number of images in the training-set.
+# This is used to pre-allocate arrays for efficiency.
+_num_images_train = _num_files_train * _images_per_file
+
+########################################################################
+# Private functions for downloading, unpacking and loading data-files.
+
+
+def download(base_url, filename, download_dir):
+    """
+    Download the given file if it does not already exist in the download_dir.
+    :param base_url: The internet URL without the filename.
+    :param filename: The filename that will be added to the base_url.
+    :param download_dir: Local directory for storing the file.
+    :return: Nothing.
+    """
+
+    # Path for local file.
+    save_path = os.path.join(download_dir, filename)
+
+    # Check if the file already exists, otherwise we need to download it now.
+    if not os.path.exists(save_path):
+        # Check if the download directory exists, otherwise create it.
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir)
+
+        print("Downloading", filename, "...")
+
+        # Download the file from the internet.
+        url = base_url + filename
+        file_path, _ = urllib.request.urlretrieve(url=url,
+                                                  filename=save_path)
+
+        print(" Done!")
+
+
+def maybe_download_and_extract(url=data_url, download_dir=hparams.data_dir):
+    """
+    Download and extract the data if it doesn't already exist.
+    Assumes the url is a tar-ball file.
+    :param url:
+        Internet URL for the tar-file to download.
+        Example: "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    :param download_dir:
+        Directory where the downloaded file is saved.
+        Example: "data/CIFAR-10/"
+    :return:
+        Nothing.
+    """
+
+    # Filename for saving the file downloaded from the internet.
+    # Use the filename from the URL and add it to the download_dir.
+    filename = url.split('/')[-1]
+    file_path = os.path.join(download_dir, filename)
+
+    # Check if the file already exists.
+    # If it exists then we assume it has also been extracted,
+    # otherwise we need to download and extract it now.
+    if not os.path.exists(file_path):
+        # Check if the download directory exists, otherwise create it.
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir)
+
+        # Download the file from the internet.
+        file_path, _ = urllib.request.urlretrieve(url=url,
+                                                  filename=file_path)
+
+        print()
+        print("Download finished. Extracting files.")
+
+        if file_path.endswith(".zip"):
+            # Unpack the zip-file.
+            zipfile.ZipFile(file=file_path, mode="r").extractall(download_dir)
+        elif file_path.endswith((".tar.gz", ".tgz")):
+            # Unpack the tar-ball.
+            tarfile.open(name=file_path, mode="r:gz").extractall(download_dir)
+
+        print("Done.")
+    else:
+        print("Data has apparently already been downloaded and unpacked.")
+
+
+def _get_file_path(filename=""):
+    """
+    Return the full path of a data-file for the data-set.
+
+    If filename=="" then return the directory of the files.
+    """
+
+    return os.path.join(hparams.data_dir, "cifar-10-batches-py/", filename)
+
+
+def _unpickle(filename):
+    """
+    Unpickle the given file and return the data.
+
+    Note that the appropriate dir-name is prepended the filename.
+    """
+
+    # Create full path for the file.
+    file_path = _get_file_path(filename)
+
+    print("Loading data: " + file_path)
+
+    with open(file_path, mode='rb') as file:
+        # In Python 3.X it is important to set the encoding,
+        # otherwise an exception is raised here.
+        data = pickle.load(file,encoding='bytes')
+
+    return data
+
+
+def _convert_images(raw):
+    """
+    Convert images from the CIFAR-10 format and
+    return a 4-dim array with shape: [image_number, height, width, channel]
+    where the pixels are floats between 0.0 and 1.0.
+    """
+
+    # Convert the raw images from the data-files to floating-points.
+    raw_float = np.array(raw, dtype=float) / 255.0
+
+    # Reshape the array to 4-dimensions.
+    images = raw_float.reshape([-1, num_channels, img_size, img_size])
+
+    # Reorder the indices of the array.
+    images = images.transpose([0, 2, 3, 1])
+
+    return images
+
+
+def _load_data(filename):
+    """
+    Load a pickled data-file from the CIFAR-10 data-set
+    and return the converted images (see above) and the class-number
+    for each image.
+    """
+
+    # Load the pickled data-file.
+    data = _unpickle(filename)
+
+    # Get the raw images.
+    raw_images = data[b'data']
+
+    # Get the class-numbers for each image. Convert to numpy-array.
+    cls = np.array(data[b'labels'])
+
+    # Convert the images.
+    images = _convert_images(raw_images)
+
+    return images, cls
+
+
+def load_class_names():
+    # Load the class-names from the pickled file.
+    raw = _unpickle(filename="batches.meta")[b'label_names']
+
+    # Convert from binary strings.
+    names = [x.decode('utf-8') for x in raw]
+
+    return names
+
+
+def load_training_data():
+    """
+    Load all the training-data for the CIFAR-10 data-set.
+
+    The data-set is split into 5 data-files which are merged here.
+
+    Returns the images, class-numbers and one-hot encoded class-labels.
+    """
+
+    # Pre-allocate the arrays for the images and class-numbers for efficiency.
+    images = np.zeros(shape=[_num_images_train, img_size, img_size, num_channels], dtype=float)
+    cls = np.zeros(shape=[_num_images_train], dtype=int)
+
+    # Begin-index for the current batch.
+    begin = 0
+
+    # For each data-file.
+    for i in range(_num_files_train):
+        # Load the images and class-numbers from the data-file.
+        images_batch, cls_batch = _load_data(filename="data_batch_" + str(i + 1))
+
+        # Number of images in this batch.
+        num_images = len(images_batch)
+
+        # End-index for the current batch.
+        end = begin + num_images
+
+        # Store the images into the array.
+        images[begin:end, :] = images_batch
+
+        # Store the class-numbers into the array.
+        cls[begin:end] = cls_batch
+
+        # The begin-index for the next batch is the current end-index.
+        begin = end
+
+    return images, cls
+
+
+def load_validation_data():
+
+    images, cls = _load_data(filename="test_batch")
+
+    images = images[5000:, :, :, :]
+    cls = cls[5000:]
+
+    return images, cls
+
+def load_testing_data():
+
+
+    images, cls = _load_data(filename="test_batch")
+
+    images = images[:5000, :, :, :]
+    cls = cls[:5000]
+
+    return images, cls
+    
+ #Network Architecture
+ def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=tf.keras.regularizers.l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+
+def resnet_v2(input_shape, depth=20, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = tf.keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(lr=hparams.learning_rate),
+                  metrics=['accuracy'])
+
+    return model
+    
+    
+ tf.logging.set_verbosity(tf.logging.DEBUG)
+
+resnet_model = resnet_v2((32, 32, 3), depth=56, num_classes=hparams.n_classes)
+print(resnet_model.summary())
+
+#Train and Fit model
+# Download and extract CIFAR-10 data
+maybe_download_and_extract()
+
+# training data
+x_train, y_train = load_training_data()
+
+# Validation data
+x_val, y_val = load_validation_data()
+
+# Testing data
+x_test, y_test = load_testing_data()
+
+# Define callbacks
+callbacks = [
+  tf.keras.callbacks.TensorBoard(log_dir=hparams.checkpoint_dir)
+]
+
+# This will do preprocessing and realtime data augmentation:
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    zca_epsilon=1e-06,  # epsilon for ZCA whitening
+    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+    # randomly shift images horizontally (fraction of total width)
+    width_shift_range=0.1,
+    # randomly shift images vertically (fraction of total height)
+    height_shift_range=0.1,
+    # set mode for filling points outside the input boundaries
+    fill_mode='nearest',
+    cval=0.,  # value used for fill_mode = "constant"
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)
+
+# Compute quantities required for feature-wise normalization
+# (std, mean, and principal components if ZCA whitening is applied).
+datagen.fit(x_train)
+
+# Fit the model on the batches generated by datagen.flow().
+history = resnet_model.fit_generator(
+    datagen.flow(x_train, y_train,batch_size=hparams.train_batch_size),
+    epochs=hparams.n_epochs,
+    validation_data=(x_val, y_val),
+    workers=4,
+    use_multiprocessing=True,
+    callbacks=callbacks)
+    
+#Results
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(history_dict['acc']) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
